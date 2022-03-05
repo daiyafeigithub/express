387e8e48ba03b6def03db015176ba87f3b8b1789
daiyafeigithub <simon.dyf@qq.com>
2022-03-05 21:21:37 +0800

    Changes

--- src/main/java/com/example/express/mapper/DataAreaMapper.java
+++ src/main/java/com/example/express/mapper/DataAreaMapper.java
@@ -3,6 +3,8 @@ package com.example.express.mapper;
 import com.baomidou.mybatisplus.core.mapper.BaseMapper;
 import com.example.express.domain.bean.DataArea;
 import com.example.express.domain.bean.SysUser;
+import org.apache.ibatis.annotations.Mapper;
 
+@Mapper
 public interface DataAreaMapper extends BaseMapper<DataArea> {
 }
